e0c84a20b0d8670b13cb0273e098bc66d7eb74ea
Reuben Simphiwe Kuse <sim.khuse@gmail.com>
2023-06-30 13:00:27 +0200

    Initial commit

---
+++ Followers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Followers
+//
+//  Created by Reuben Simphiwe Kuse on 2023/06/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
